1393c60f25f5a1ad49b4d3d539cc3fe3de6ae1b4
Muhammad Abdullah <100672137+AbdullahProgrammer426351@users.noreply.github.com>
2025-08-29 11:13:31 +0500

    First commit

---
+++ Sources/LayoutPackage/File.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  LayoutPackage
+//
+//  Created by APPLE on 29/08/2025.
+//
+
+import SwiftUI
+
+struct MagicalText: View {
+    var body: some View {
+        Text("This is magical text view")
+    }
+}
